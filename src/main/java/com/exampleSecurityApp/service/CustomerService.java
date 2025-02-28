1c870b086cf8e247175daf7d8391608775ff6ec1
suman043 <suman.mohanty043@gmail.com>
2025-02-28 17:14:03 +0530

    updating service class

--- src/main/java/com/exampleSecurityApp/service/CustomerService.java
+++ src/main/java/com/exampleSecurityApp/service/CustomerService.java
@@ -1,4 +1,26 @@
 package com.exampleSecurityApp.service;
 
+import com.exampleSecurityApp.entity.Customer;
+import com.exampleSecurityApp.repo.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CustomerService {
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public boolean saveCustomer(Customer customer){
+
+        String encodedPwd = bCryptPasswordEncoder.encode(customer.getPwd());
+        customer.setPwd(encodedPwd);
+
+        Customer savedCustomer = customerRepository.save(customer);
+        return savedCustomer.getCid()!=null;
+    }
 }
