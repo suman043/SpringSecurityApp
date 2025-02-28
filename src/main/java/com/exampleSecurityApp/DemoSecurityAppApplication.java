524eb5a8825e9f9464528152614ca7ff7b5509f5
suman043 <suman.mohanty043@gmail.com>
2025-02-28 15:38:18 +0530

    Initial Program

---
+++ src/main/java/com/exampleSecurityApp/DemoSecurityAppApplication.java
@@ -0,0 +1,13 @@
+package com.exampleSecurityApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSecurityAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSecurityAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleSecurityApp/DemoSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
