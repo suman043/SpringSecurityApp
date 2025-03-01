6f793a18411e336798cf6ad2c8ee8f13c40a36b2
suman043 <suman.mohanty043@gmail.com>
2025-03-01 12:56:22 +0530

    Implementing user login from db with proper security authentication

--- src/main/java/com/exampleSecurityApp/config/AppSecurityConfig.java
+++ src/main/java/com/exampleSecurityApp/config/AppSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.exampleSecurityApp.config;
 
+import com.exampleSecurityApp.service.CustomerService;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -11,6 +16,26 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new
+                DaoAuthenticationProvider();
+
+        authenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        authenticationProvider.setUserDetailsService(customerService);
+
+        return authenticationProvider;
+    }
+
+    @Bean
+    @SneakyThrows
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration){
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
@@ -21,11 +46,11 @@ public class AppSecurityConfig {
     @SneakyThrows
     public SecurityFilterChain securityFilterChain(HttpSecurity http){
 
-        //logic
+        //logic for filter chain
         http
                         .csrf(request->request.disable())
                         .authorizeHttpRequests(request-> request
-                        .requestMatchers("/register")
+                        .requestMatchers("/register","/login")
                         .permitAll()
                         .anyRequest()
                         .authenticated());
--- src/main/java/com/exampleSecurityApp/controller/CustomerRestController.java
+++ src/main/java/com/exampleSecurityApp/controller/CustomerRestController.java
@@ -5,6 +5,9 @@ import com.exampleSecurityApp.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,9 @@ public class CustomerRestController {
 
     @Autowired
     private CustomerService customerService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
 
     @PostMapping("/register")
     public ResponseEntity<String> registerCustomer(@RequestBody Customer customer){
@@ -28,4 +34,21 @@ public class CustomerRestController {
         }
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody Customer customer){
+
+        UsernamePasswordAuthenticationToken token = new
+                UsernamePasswordAuthenticationToken(customer.getEmail(), customer.getPwd());
+
+        //verify login details valid or not
+        Authentication authenticate = authenticationManager.authenticate(token);
+
+        boolean authenticatedStatus = authenticate.isAuthenticated();
+        if(authenticatedStatus){
+            return new ResponseEntity<>("Welcome",HttpStatus.OK);
+        }
+        else{
+            return new ResponseEntity<>("Failed", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
